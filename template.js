6cff667be40914ed635ebaf18b86010ea000a186
Brian <briankeefe@umass.edu>
2019-11-18 16:10:26 +0000

    solved the first problem and created a template

---
+++ template.js
@@ -0,0 +1,13 @@
+/*
+    A template for making practice problems to solve
+*/
+
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Ask a question... ", () => {
+});
