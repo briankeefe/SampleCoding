e9e94e42b56423bf848ffc809c7fd5db76a1ad2f
Brian <briankeefe@umass.edu>
2019-11-18 16:53:15 +0000

    Added a formatter, needs tweaking

--- countVowels.js
+++ countVowels.js
@@ -1,47 +1,46 @@
 /*
-    A template for making practice problems to solve
+    Count the number of each vowel in the string
 */
 
-
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question("Enter a string to count the vowels... ", (s) => {
-    let tally = vowelCounter(s.toLowerCase());
-    console.log(tally);
-});
-
-// const vowels = ['a', 'e', 'i', 'o', 'u']
-
-class Tally{
-    constructor() {
-        this.a = 0;
-        this.e = 0;
-        this.i = 0;
-        this.o = 0;
-        this.u = 0;
-    }
-
+class Tally {
+  constructor() {
+    this.a = 0;
+    this.e = 0;
+    this.i = 0;
+    this.o = 0;
+    this.u = 0;
+  }
 }
 
 const vowelCounter = (str) => {
-    let t = new Tally();
-    for (let i = 0; i < str.length; ++i){
-        let cur = str.charAt(i)
-        if (cur === 'a') {
-            t.a++;
-        } else if (cur === 'e') {
-            t.e++;
-        } else if (cur === 'i') {
-            t.i++;
-        } else if (cur === 'o') {
-            t.o++;
-        } else if (cur === 'u') {
-            t.u++;
-        }
+  const t = new Tally();
+  for (let i = 0; i < str.length; i += 1) {
+    const cur = str.charAt(i);
+    if (cur === 'a') {
+      t.a += 1;
+    } else if (cur === 'e') {
+      t.e += 1;
+    } else if (cur === 'i') {
+      t.i += 1;
+    } else if (cur === 'o') {
+      t.o += 1;
+    } else if (cur === 'u') {
+      t.u += 1;
     }
-    return t;
-}
+  }
+  return t;
+};
+
+const readline = require('readline')
+;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Enter a string to count the vowels... ', (s) => {
+  const tally = vowelCounter(s.toLowerCase());
+  console.log(tally);
+});
+
+// const vowels = ['a', 'e', 'i', 'o', 'u']
